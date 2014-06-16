13ae841faa88c5457defed0487438469a8051ad0
Kan Fushihara <kan.fushihara@gmail.com>
2014-06-16 13:36:29 +0900

    Checking in changes prior to tagging of version 0.07.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 0cfeda1..20ca11d 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,9 @@ Revision history for Perl extension Devel-Cover-Report-Coveralls
    
     {{$NEXT}}
    
    +0.07 2014-06-16T04:36:24Z
    +    - depend JSON::XS to JSON::PP (thanks fioux, karenetheridge)
    +
     0.06 2013-07-14T13:23:11Z
         - fixed pod (thanks dsteinbrunner)

--- lib/Devel/Cover/Report/Coveralls.pm
+++ lib/Devel/Cover/Report/Coveralls.pm
@@ -2,7 +2,7 @@ package Devel::Cover::Report::Coveralls;
 use strict;
 use warnings;
 use 5.008005;
-our $VERSION = "0.06";
+our $VERSION = "0.07";
 
 our $CONFIG_FILE = '.coveralls.yml';
 our $API_ENDPOINT = 'https://coveralls.io/api/v1/jobs';
